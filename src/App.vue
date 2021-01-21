08f31cf0a94e9d47d5a42d2a8665bb43dd892482
WojtasBjj <wojtekgajda94@gmail.com>
2021-01-21 15:59:31 +0100

    welcomepage and some css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
+    <welcome-page></welcome-page>
     <router-view></router-view>
     <the-footer></the-footer>
   </div>
@@ -9,11 +10,13 @@
 <script>
 import TheHeader from './components/layout/TheHeader.vue'
 import TheFooter from './components/layout/TheFooter.vue'
+import WelcomePage from './components/pages/WelcomePage.vue'
 
 export default {
   components: {
     TheHeader,
-    TheFooter
+    TheFooter,
+    WelcomePage
   }
 }
 </script>
---
+++ src/components/pages/WelcomePage.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="bg-gray-100 h-96 w-auto justify-center">
+    <div class="">
+        <h1 class="">Witaj na stronie HomeCraftApp!</h1>
+        <p>ta strona jest widoczna tylko dla NIEZALOGOWANYCH użtkowników</p>
+    </div>
+</div>
+</template>
